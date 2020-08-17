084b4188f7150482f59ad8e1e930f596c9797fc7
yuanwei <gsyuanwei@163.com>
2020-08-17 10:31:40 +0800

    注册Vuex/store

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import store from './store'
+
 Vue.config.productionTip = false
 
 //创建事件总线实例
@@ -10,4 +12,5 @@ Vue.prototype.$bus = new Vue()
 new Vue({
   render: h => h(App),
   router,
+  store
 }).$mount('#app')
